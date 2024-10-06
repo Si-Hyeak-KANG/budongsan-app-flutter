bde690d9199044b69e5c4e833c511d6de796559b
Si-Hyeak-KANG <zlcls456@gmail.com>
2024-10-06 17:18:44 +0900

    검색 필터 개발

--- lib/map/map_page.dart
+++ lib/map/map_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'map_filter.dart';
+import 'map_filter_dialog.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({super.key});
@@ -10,6 +12,8 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPage extends State<MapPage> {
+  int currentItem = 0;
+  MapFilter mapFilter = MapFilter();
 
   @override
   void initState() {
@@ -18,10 +22,85 @@ class _MapPage extends State<MapPage> {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return const Scaffold(
-      body: Center(
-        child: Text('test'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My 부동산'),
+        actions:[
+          IconButton(
+              onPressed: () async {
+                var result = await Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (context) {
+                      return MapFilterDialog(mapFilter);
+                }));
+                if(result != null) {
+                  mapFilter = result as MapFilter; // MapFilterDialog 에서 반환한 결과를 MapFilter Type Casting 하고, 현재 mapFilter 변수에 저장
+                }
+              },
+              icon: const Icon(Icons.search))
+        ],
+      ),
+      drawer: Drawer( // side menu
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    '홍길동',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Text(
+                    'hong@gmail.com',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            ListTile(
+              title: const Text('내가 선택한 아파트'),
+              onTap: () {},
+            ),
+            ListTile(
+              title: const Text('설정'),
+              onTap: () {},
+            ),
+          ],
+        ),
+      ),
+      body: currentItem == 0 ? Container() : ListView(),
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: currentItem,
+          onTap: (value) {
+            setState(() {
+              currentItem = value;
+            });
+          },
+          items: const [
+            BottomNavigationBarItem(
+              label: 'map',
+              icon: Icon((Icons.map)),
+            ),
+            BottomNavigationBarItem(
+              label: 'list',
+              icon: Icon((Icons.list)),
+            ),
+          ],
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+          onPressed: () {},
+          label: const Text('이 위치로 검색하기'),
       ),
     );
   }
